c67158fc1bd0af04cd9644db68599e380b58b9f1
afolabiopakunle <opakunleafolabi@gmail.com>
2022-08-30 13:31:14 +0100

    service created

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
+import { PostService } from '../services.ts/post.service';
 
 interface IPost {
   body: string;
@@ -16,56 +17,49 @@ export class PostComponent implements OnInit {
 
   posts: IPost[];
 
-  constructor(private http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
-    .subscribe((response: IPost[]) => {
-      this.posts = response
-    })
+  constructor(private postService: PostService) {
+
   }
 
   ngOnInit() {
-    // this.service.getAll()
-    //   .subscribe(posts => this.posts = posts);
+    this.postService.getPosts()
+    .subscribe((response: IPost[]) => {
+      this.posts = response
+    });
   }
 
   createPost(input: HTMLInputElement) {
-    let post = { title: input.value };
+    let post =  {
+      title: input.value, 
+      id: 0, 
+      body: '', 
+      userId: 0
+    }
+    console.log(post)
     input.value = '';
-
-    // this.service.create(post)
-    //   .subscribe(
-    //     newPost => {
-    //       post['id'] = newPost.id;
-    //         this.posts.splice(0, 0, post);
-    //       },
-    //       (error: AppError) => {
-    //         if (error instanceof BadInput) {
-    //           // this.form.setErrors(error.originalError);
-    //         }
-    //         else throw error;
-    //       });
+      // this.http.post(this.url, post)
+      // .subscribe((response:IPost) => {
+      //   console.log(response)
+      //   post['title'] = response.title
+      //   post['id'] = response.id
+      //   post['body'] = ''
+      //   post['userId'] = 0
+      //   this.posts.unshift(post)
+      // })
   }
 
-  // updatePost(post) {
-  //   this.service.update(post)
-  //     .subscribe(
-  //       updatedPost => {
-  //         console.log(updatedPost);
-  //       });
-  // }
+  updatePost(post) {
+    // this.http.patch(this.url + '/' + post.id, {isRead: true})
+    // .subscribe(response => {
+    //   console.log(response)
+    // })
+  }
 
   deletePost(post) {
-    // this.service.delete(post.id)
-      // .subscribe(
-        // () => {
-        //   let index = this.posts.indexOf(post);
-        //   this.posts.splice(index, 1);
-        // },
-        // (error: AppError) => {
-        //   if (error instanceof NotFoundError)
-        //     alert('This post has already been deleted.');
-        //   else throw error;
-        // });
+    // this.http.delete(this.url + '/' + post.id)
+    // .subscribe(response => {
+    //   let index = this.posts.indexOf(post)
+    //   this.posts.splice(index, 1)
+    // })
   }
-
 }
