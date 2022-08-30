b61d82561d87fff8ed48fe635aacc983296e1205
afolabiopakunle <opakunleafolabi@gmail.com>
2022-08-30 10:27:11 +0100

    Initial commit

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  posts: any[];
+
+  constructor(private service: PostService) {
+  }
+
+  ngOnInit() {
+    this.service.getAll()
+      .subscribe(posts => this.posts = posts);
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+
+    this.service.create(post)
+      .subscribe(
+        newPost => {
+          post['id'] = newPost.id;
+            this.posts.splice(0, 0, post);
+          },
+          (error: AppError) => {
+            if (error instanceof BadInput) {
+              // this.form.setErrors(error.originalError);
+            }
+            else throw error;
+          });
+  }
+
+  updatePost(post) {
+    this.service.update(post)
+      .subscribe(
+        updatedPost => {
+          console.log(updatedPost);
+        });
+  }
+
+  deletePost(post) {
+    this.service.delete(post.id)
+      .subscribe(
+        () => {
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+        },
+        (error: AppError) => {
+          if (error instanceof NotFoundError)
+            alert('This post has already been deleted.');
+          else throw error;
+        });
+  }
+
+}
