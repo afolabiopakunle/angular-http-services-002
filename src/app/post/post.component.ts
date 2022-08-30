ee039f2f4f9936e082d778d91dd3ea4a7ce8057d
afolabiopakunle <opakunleafolabi@gmail.com>
2022-08-30 10:31:33 +0100

    3 files modified, 1 added

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -9,52 +9,52 @@ export class PostComponent implements OnInit {
 
   posts: any[];
 
-  constructor(private service: PostService) {
+  constructor() {
   }
 
   ngOnInit() {
-    this.service.getAll()
-      .subscribe(posts => this.posts = posts);
+    // this.service.getAll()
+    //   .subscribe(posts => this.posts = posts);
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = '';
 
-    this.service.create(post)
-      .subscribe(
-        newPost => {
-          post['id'] = newPost.id;
-            this.posts.splice(0, 0, post);
-          },
-          (error: AppError) => {
-            if (error instanceof BadInput) {
-              // this.form.setErrors(error.originalError);
-            }
-            else throw error;
-          });
+    // this.service.create(post)
+    //   .subscribe(
+    //     newPost => {
+    //       post['id'] = newPost.id;
+    //         this.posts.splice(0, 0, post);
+    //       },
+    //       (error: AppError) => {
+    //         if (error instanceof BadInput) {
+    //           // this.form.setErrors(error.originalError);
+    //         }
+    //         else throw error;
+    //       });
   }
 
-  updatePost(post) {
-    this.service.update(post)
-      .subscribe(
-        updatedPost => {
-          console.log(updatedPost);
-        });
-  }
+  // updatePost(post) {
+  //   this.service.update(post)
+  //     .subscribe(
+  //       updatedPost => {
+  //         console.log(updatedPost);
+  //       });
+  // }
 
-  deletePost(post) {
-    this.service.delete(post.id)
-      .subscribe(
-        () => {
-          let index = this.posts.indexOf(post);
-          this.posts.splice(index, 1);
-        },
-        (error: AppError) => {
-          if (error instanceof NotFoundError)
-            alert('This post has already been deleted.');
-          else throw error;
-        });
-  }
+  // deletePost(post) {
+  //   this.service.delete(post.id)
+  //     .subscribe(
+  //       () => {
+  //         let index = this.posts.indexOf(post);
+  //         this.posts.splice(index, 1);
+  //       },
+  //       (error: AppError) => {
+  //         if (error instanceof NotFoundError)
+  //           alert('This post has already been deleted.');
+  //         else throw error;
+  //       });
+  // }
 
 }
