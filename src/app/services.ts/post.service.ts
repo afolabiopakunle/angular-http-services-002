b7fa8cc712a81d73b5df409df1fa114da7c543d7
afolabiopakunle <opakunleafolabi@gmail.com>
2022-08-30 19:01:11 +0100

    throw error fixed

--- src/app/services.ts/post.service.ts
+++ src/app/services.ts/post.service.ts
@@ -30,6 +30,6 @@ export class PostService {
   }
 
   errorHandler(error: HttpErrorResponse) {
-    return Observable.throw(() => error.message || "server error.");
+    return throwError(() => error.message || "server error.");
   }
 }
